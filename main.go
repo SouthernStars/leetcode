ef161129a592e6fec5fdc8acf89654790780c9d7
zhangruipeng <gzhangruipeng@gmail.com>
2025-01-09 11:59:41 +0800

    99.岛屿数量 200.岛屿数量

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var directory = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 上下左右
+
 func main() {
 	var n, m int
 	fmt.Scanf("%d %d", &n, &m)
@@ -19,24 +21,27 @@ func main() {
 	cnt := 0
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
-			if grid[i][j] == 1 && vist[i][j] == false {
+			if grid[i][j] == 1 && !vist[i][j] {
 				cnt++
-				vist[i][j] = true
-				dfs(grid, &vist, i, j)
+				bfs(grid, &vist, i, j)
 			}
 		}
 	}
 	fmt.Println(cnt)
 }
 
-var directory = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 上下左右
-
-func dfs(grid [][]int, vist *[][]bool, x, y int) {
-	for _, dir := range directory {
-		newX, newY := x+dir[0], y+dir[1]
-		if newX >= 0 && newX < len(grid) && newY >= 0 && newY < len(grid[0]) && grid[newX][newY] == 1 && (*vist)[newX][newY] == false {
-			(*vist)[newX][newY] = true
-			dfs(grid, vist, newX, newY)
+func bfs(grid [][]int, vist *[][]bool, x, y int) {
+	queue := make([][]int, 0) // 辅助队列
+	queue = append(queue, []int{x, y})
+	for len(queue) > 0 {
+		e := queue[0] // 出队
+		queue = queue[1:]
+		for _, dir := range directory {
+			newX, newY := e[0]+dir[0], e[1]+dir[1]
+			if newX >= 0 && newX < len(grid) && newY >= 0 && newY < len(grid[0]) && grid[newX][newY] == 1 && (*vist)[newX][newY] == false {
+				queue = append(queue, []int{newX, newY})
+				(*vist)[newX][newY] = true
+			}
 		}
 	}
 }

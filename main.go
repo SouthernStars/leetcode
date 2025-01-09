4f5275eb8d55ab9df2165b23eebe47d6cc44e6a7
zhangruipeng <gzhangruipeng@gmail.com>
2025-01-09 14:24:46 +0800

    100.岛屿的最大面积

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
-var directory = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 上下左右
-
+// 4 5
+// 1 1 0 0 0
+// 1 1 0 0 0
+// 0 0 1 0 0
+// 0 0 0 1 1
 func main() {
 	var n, m int
 	fmt.Scanf("%d %d", &n, &m)
@@ -18,30 +21,45 @@ func main() {
 			fmt.Scanf("%d", &grid[i][j])
 		}
 	}
-	cnt := 0
+	ans := 0
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
 			if grid[i][j] == 1 && !vist[i][j] {
-				cnt++
-				bfs(grid, &vist, i, j)
+				cnt := 0
+				bfs(grid, &vist, &cnt, i, j)
+				ans = max(ans, cnt)
 			}
 		}
 	}
-	fmt.Println(cnt)
+	fmt.Println(ans)
 }
 
-func bfs(grid [][]int, vist *[][]bool, x, y int) {
-	queue := make([][]int, 0) // 辅助队列
-	queue = append(queue, []int{x, y})
+// 上下左右
+var directions = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+func bfs(grid [][]int, vist *[][]bool, cnt *int, x, y int) {
+	var queue [][2]int
+	queue = append(queue, [2]int{x, y})
+	(*vist)[x][y] = true
+	*cnt++
+
 	for len(queue) > 0 {
-		e := queue[0] // 出队
-		queue = queue[1:]
-		for _, dir := range directory {
+		e := queue[0]
+		queue = queue[1:] // 出队
+		for _, dir := range directions {
 			newX, newY := e[0]+dir[0], e[1]+dir[1]
 			if newX >= 0 && newX < len(grid) && newY >= 0 && newY < len(grid[0]) && grid[newX][newY] == 1 && (*vist)[newX][newY] == false {
-				queue = append(queue, []int{newX, newY})
 				(*vist)[newX][newY] = true
+				*cnt++
+				queue = append(queue, [2]int{newX, newY})
 			}
 		}
 	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }

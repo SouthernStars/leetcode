1e9c85558fdec45fd7c4dc580e1a8215f2334810
zhangruipeng <gzhangruipeng@gmail.com>
2025-01-09 15:24:33 +0800

    101.孤岛的总面积

--- main.go
+++ main.go
@@ -2,11 +2,6 @@ package main
 
 import "fmt"
 
-// 4 5
-// 1 1 0 0 0
-// 1 1 0 0 0
-// 0 0 1 0 0
-// 0 0 0 1 1
 func main() {
 	var n, m int
 	fmt.Scanf("%d %d", &n, &m)
@@ -21,16 +16,40 @@ func main() {
 			fmt.Scanf("%d", &grid[i][j])
 		}
 	}
+	// 统计非孤岛，并且标记
+	cnt := 0
+	for i := 0; i < n; i++ {
+		if grid[i][0] == 1 && !vist[i][0] {
+			cnt++
+			vist[i][0] = true
+			dfs(grid, &vist, &cnt, i, 0)
+		}
+		if grid[i][m-1] == 1 && !vist[i][m-1] {
+			cnt++
+			vist[i][m-1] = true
+			dfs(grid, &vist, &cnt, i, m-1)
+		}
+	}
+	for i := 0; i < m; i++ {
+		if grid[0][i] == 1 && !vist[0][i] {
+			cnt++
+			vist[0][i] = true
+			dfs(grid, &vist, &cnt, 0, i)
+		}
+		if grid[n-1][i] == 1 && !vist[n-1][i] {
+			cnt++
+			vist[n-1][i] = true
+			dfs(grid, &vist, &cnt, n-1, i)
+		}
+	}
+	// 然后再遍历一遍，统计孤岛
 	ans := 0
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
 			if grid[i][j] == 1 && !vist[i][j] {
-				cnt := 0
-				ise := false
-				bfs(grid, &vist, &cnt, &ise, i, j)
-				if !ise {
-					ans += cnt
-				}
+				ans++
+				vist[i][j] = true
+				dfs(grid, &vist, &ans, i, j)
 			}
 		}
 	}
@@ -40,28 +59,13 @@ func main() {
 // 上下左右
 var directions = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
-func bfs(grid [][]int, vist *[][]bool, cnt *int, ise *bool, x, y int) {
-	var queue [][2]int
-	queue = append(queue, [2]int{x, y})
-	(*vist)[x][y] = true
-	*cnt++
-	if x == 0 || y == 0 || x == len(grid)-1 || y == len(grid[0])-1 {
-		*ise = true
-	}
-
-	for len(queue) > 0 {
-		e := queue[0]
-		queue = queue[1:] // 出队
-		for _, dir := range directions {
-			newX, newY := e[0]+dir[0], e[1]+dir[1]
-			if newX >= 0 && newX < len(grid) && newY >= 0 && newY < len(grid[0]) && grid[newX][newY] == 1 && (*vist)[newX][newY] == false {
-				(*vist)[newX][newY] = true
-				*cnt++
-				if newX == 0 || newY == 0 || newX == len(grid)-1 || newY == len(grid[0])-1 {
-					*ise = true
-				}
-				queue = append(queue, [2]int{newX, newY})
-			}
+func dfs(grid [][]int, vist *[][]bool, cnt *int, x, y int) {
+	for _, dir := range directions {
+		newX, newY := x+dir[0], y+dir[1]
+		if newX >= 0 && newX < len(grid) && newY >= 0 && newY < len(grid[0]) && grid[newX][newY] == 1 && (*vist)[newX][newY] == false {
+			(*vist)[newX][newY] = true
+			*cnt++
+			dfs(grid, vist, cnt, newX, newY)
 		}
 	}
 }

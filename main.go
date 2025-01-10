4b21e1f5d8cde3d660d4490d91e77650ff05aec7
zhangruipeng <gzhangruipeng@gmail.com>
2025-01-10 14:44:58 +0800

    110.字符串接龙

--- main.go
+++ main.go
@@ -2,35 +2,41 @@ package main
 
 import "fmt"
 
-// 上下左右
-var direction = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-
 func main() {
-	var n, m int
-	fmt.Scanf("%d %d", &n, &m)
-	grid := make([][]int, n)
-	for i := range grid {
-		grid[i] = make([]int, m)
+	var n int
+	fmt.Scanf("%d", &n)
+	var startStr, endStr string
+	fmt.Scanf("%s %s", &startStr, &endStr)
+
+	strSet := map[string]bool{}
+	for i := 1; i <= n; i++ {
+		var tmp string
+		fmt.Scanf("%s", &tmp)
+		strSet[tmp] = true
 	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			fmt.Scanf("%d", &grid[i][j])
-		}
-	}
-	cnt := 0
-	// 按层顺序遍历
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			if grid[i][j] == 1 {
-				// 统计该节点上下左右的水域
-				for _, dir := range direction {
-					newX, newY := i+dir[0], j+dir[1]
-					if newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == 0 {
-						cnt++
-					}
+	// 广度优先遍历
+	vistedMap := map[string]int{}
+	queue := []string{startStr}
+	vistedMap[startStr] = 1
+	for len(queue) > 0 {
+		top := queue[0]
+		queue = queue[1:] // 队头出队
+		path := vistedMap[top]
+		for i := 0; i < len(top); i++ { // 一个一个替换字符
+			newWord := []byte(top)
+			for j := 0; j < 26; j++ {
+				newWord[i] = 'a' + byte(j)
+				word := string(newWord)
+				if word == endStr {
+					fmt.Println(path + 1)
+					return
+				}
+				if strSet[word] && vistedMap[word] == 0 {
+					vistedMap[word] = path + 1
+					queue = append(queue, word)
 				}
 			}
 		}
 	}
-	fmt.Println(cnt)
+	fmt.Println(0)
 }

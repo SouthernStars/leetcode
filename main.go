dbe964948c4f8d94d9c478a4f64453ff58be584b
zhangruipeng <gzhangruipeng@gmail.com>
2025-01-10 12:15:04 +0800

    106.岛屿的周长

--- main.go
+++ main.go
@@ -2,37 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	// 使用邻接表存储
-	var n, k int
-	fmt.Scanf("%d %d", &n, &k)
-	graph := make([][]int, n+1) // 使用 1~n
-	for i := 0; i <= n; i++ {
-		graph[i] = make([]int, 0)
-	}
-	// 访问标记数组
-	visited := make([]bool, n+1)
-	// 存储边
-	for i := 1; i <= k; i++ {
-		var s, e int
-		fmt.Scanf("%d %d", &s, &e)
-		graph[s] = append(graph[s], e)
-	}
-	cnt := 0
-	dfs(graph, &visited, 1, &cnt)
-	if cnt == n {
-		fmt.Println(1)
-	} else {
-		fmt.Println(-1)
-	}
-}
+// 上下左右
+var direction = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
-func dfs(graph [][]int, visited *[]bool, cur int, cnt *int) {
-	(*visited)[cur] = true
-	*cnt++
-	for _, e := range graph[cur] {
-		if !(*visited)[e] {
-			dfs(graph, visited, e, cnt)
+func main() {
+	var n, m int
+	fmt.Scanf("%d %d", &n, &m)
+	grid := make([][]int, n)
+	for i := range grid {
+		grid[i] = make([]int, m)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			fmt.Scanf("%d", &grid[i][j])
 		}
 	}
+	cnt := 0
+	// 按层顺序遍历
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == 1 {
+				// 统计该节点上下左右的水域
+				for _, dir := range direction {
+					newX, newY := i+dir[0], j+dir[1]
+					if newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == 0 {
+						cnt++
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(cnt)
 }

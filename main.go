9ccc3929df2f15ff272cc4c47de19f3d7920e49f
zhangruipeng <gzhangruipeng@gmail.com>
2025-01-13 15:38:53 +0800

    109.冗余连接II

--- main.go
+++ main.go
@@ -8,16 +8,47 @@ func main() {
 	var N int
 	fmt.Scanf("%d", &N)
 	father = make([]int, N+1)
-	Init(N)
+	indegree := make([]int, N+1)
+	edges := make([][2]int, N)
+
+	// 统计每个结点的入度
 	for i := 0; i < N; i++ {
-		var start, end int
-		fmt.Scanf("%d %d", &start, &end)
-		if IsSameRoot(start, end) {
-			fmt.Printf("%d %d\n", start, end)
-			return
-		} else {
-			Join(start, end)
+		fmt.Scanf("%d %d", &edges[i][0], &edges[i][1])
+		indegree[edges[i][1]]++
+	}
+	// 统计入度为2 的点及关联的边，按照逆序
+	var twoEdges []int
+	for i := N - 1; i >= 0; i-- {
+		if indegree[edges[i][1]] == 2 {
+			twoEdges = append(twoEdges, i)
 		}
+	}
+
+	if len(twoEdges) == 0 { // 存在环
+		Init(N)
+		for i := 0; i < N; i++ {
+			if IsSameRoot(edges[i][0], edges[i][1]) {
+				fmt.Printf("%d %d\n", edges[i][0], edges[i][1])
+				return
+			} else {
+				Join(edges[i][0], edges[i][1])
+			}
+		}
+	} else { // 存在入度为 2 的点
+		deleteEdge := twoEdges[0]
+		Init(N)
+		for i := 0; i < N; i++ {
+			if i == deleteEdge {
+				continue
+			}
+			if IsSameRoot(edges[i][0], edges[i][1]) { // 删除边后，构成了环，因此删错了, 修改删除边
+				deleteEdge = twoEdges[1]
+				break
+			} else {
+				Join(edges[i][0], edges[i][1])
+			}
+		}
+		fmt.Printf("%d %d\n", edges[deleteEdge][0], edges[deleteEdge][1])
 	}
 }
 

1ecc745f87e51d7437384b3a7767170c697f124c
zhangruipeng <gzhangruipeng@gmail.com>
2025-03-04 23:40:43 +0800

    399.除法求值

--- main.go
+++ main.go
@@ -2,80 +2,40 @@ package main
 
 import "fmt"
 
-var father []int
-
 func main() {
 	var N int
 	fmt.Scanf("%d", &N)
-	father = make([]int, N+1)
-	indegree := make([]int, N+1)
-	edges := make([][2]int, N)
 
-	// 统计每个结点的入度
+	arr := make([]int, N)
+	sum := 0
 	for i := 0; i < N; i++ {
-		fmt.Scanf("%d %d", &edges[i][0], &edges[i][1])
-		indegree[edges[i][1]]++
-	}
-	// 统计入度为2 的点及关联的边，按照逆序
-	var twoEdges []int
-	for i := N - 1; i >= 0; i-- {
-		if indegree[edges[i][1]] == 2 {
-			twoEdges = append(twoEdges, i)
-		}
+		fmt.Scanf("%d", &arr[i])
+		sum += arr[i]
 	}
 
-	if len(twoEdges) == 0 { // 存在环
-		Init(N)
-		for i := 0; i < N; i++ {
-			if IsSameRoot(edges[i][0], edges[i][1]) {
-				fmt.Printf("%d %d\n", edges[i][0], edges[i][1])
-				return
-			} else {
-				Join(edges[i][0], edges[i][1])
+	// sum = 红 + 蓝 + 白 => 红 = 蓝 = （sum - 白）/2
+	// 动态规划  dp[i][j] 表示 前 i 个数中 红 -蓝 = j 的方案数，范围 -S S  由于不支持 统一 +S
+	// 初始化 dp[0][sum] = 1
+
+	dp := make([][]int, N+1)
+	for i := 0; i <= N; i++ {
+		dp[i] = make([]int, 2*sum+1)
+	}
+
+	dp[0][sum] = 1 // 表示从 前 0 个钟选择 红-蓝 = 0 的方案数 为 1
+	// 遍历物品
+	for i := 1; i <= N; i++ {
+		for j := 0; j <= 2*sum; j++ {
+			// 如果当前不涂色 + 涂红 + 涂蓝
+			dp[i][j] = dp[i-1][j]
+			if j-arr[i-1] >= 0 {
+				dp[i][j] += dp[i-1][j-arr[i-1]]
+			}
+			if j+arr[i-1] <= 2*sum {
+				dp[i][j] += dp[i-1][j+arr[i-1]]
 			}
 		}
-	} else { // 存在入度为 2 的点
-		deleteEdge := twoEdges[0]
-		Init(N)
-		for i := 0; i < N; i++ {
-			if i == deleteEdge {
-				continue
-			}
-			if IsSameRoot(edges[i][0], edges[i][1]) { // 删除边后，构成了环，因此删错了, 修改删除边
-				deleteEdge = twoEdges[1]
-				break
-			} else {
-				Join(edges[i][0], edges[i][1])
-			}
-		}
-		fmt.Printf("%d %d\n", edges[deleteEdge][0], edges[deleteEdge][1])
 	}
-}
-
-// Init 初始化并查集，编号1~n
-func Init(n int) {
-	for i := 1; i <= n; i++ {
-		father[i] = i
-	}
-}
-
-// FindRoot 寻找 u 的根
-func FindRoot(u int) int {
-	if u == father[u] {
-		return u
-	}
-	father[u] = FindRoot(father[u])
-	return father[u]
-}
-
-func Join(u, v int) {
-	u = FindRoot(u)
-	v = FindRoot(v)
-	if u != v {
-		father[v] = u
-	}
-}
-
-func IsSameRoot(u, v int) bool {
-	return FindRoot(u) == FindRoot(v)
+	// -1 是因为全不涂
+	fmt.Println(dp[N][sum] - 1)
 }

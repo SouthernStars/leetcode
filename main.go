cbf1c3b89ea573354b305b365dbd424daf7291e9
zhangruipeng <gzhangruipeng@gmail.com>
2025-04-16 20:39:59 +0800

    399.除法求值

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func producer(ch chan<- int) {
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second) // 模拟生产数据
-		fmt.Println("producer 生产数据", i)
-		ch <- i
-	}
-	close(ch)
-}
-
 func main() {
 
 }
